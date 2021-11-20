765f548e9b6ff6527d29a0a4eb75b86b1407dba6
Juan Tarquino <jptarqu@gmail.com>
2021-11-20 13:16:08 -0500

    Fix encoding and multiple lines output issue

--- FileSplit/FileSplit.Core/Core.fs
+++ FileSplit/FileSplit.Core/Core.fs
@@ -7,34 +7,45 @@ open FileSplit.Core.Types
 
 
 module Splitter =
+    let private createNewFile (folderPicked:IFolderPicked) outputFolder outputFilepath encodingFound currFileNumber =
+        async {
+            let newFileName = Path.GetFileNameWithoutExtension(outputFilepath) + "-" + currFileNumber.ToString() + Path.GetExtension(outputFilepath)
+            let! newStream = folderPicked.CreateFile newFileName |> Async.AwaitTask
+            let newFilepath = Path.Combine( outputFolder, newFileName)
+            let fileWriter = new StreamWriter(newStream, encodingFound)
+            return (fileWriter, newFilepath)
+        }
+        
+
     let SplitFile currFileNotifier (inputStream:Stream) (folderPicked:IFolderPicked) (outputFileName:string) (maxLinesPerFile:int)  =
         async {
             let filesCreated = List<string>()
-            let outputFolder = folderPicked.GetFolderPath() //C:\Users\<user>\AppData\Local\Packages\f33bd753-7cb6-4177-bf50-b993ed286c3b_dcy57zvw0vndm\LocalState
-            let! outStream = folderPicked.CreateFile outputFileName |> Async.AwaitTask
+            let outputFolder = folderPicked.GetFolderPath() 
             let outputFilepath = Path.Combine( outputFolder, outputFileName)
-            let mutable fileWriter = new StreamWriter(outStream)
-            filesCreated.Add(outputFilepath)
             use fileReader = new StreamReader(inputStream, true)
+            let encodingFound = fileReader.CurrentEncoding
+            let mutable fileWriter = null
+            filesCreated.Add(outputFilepath)
             let mutable currLineNumber = 0
             let mutable currFileNumber = 1
+            let createNewSplitFile = createNewFile folderPicked outputFolder outputFilepath encodingFound
             try 
                 let mutable currLine = fileReader.ReadLine()
                 while currLine |> isNull |> not do
-                    if currLineNumber > 0 then 
-                        fileWriter.WriteLine(currLine)
+                    match currLineNumber with
+                    | 0 ->
+                        let! (newStream,newFilepath) = createNewSplitFile currFileNumber
+                        fileWriter <- newStream
+                        filesCreated.Add(newFilepath)
+                        currFileNotifier  currFileNumber
+                    | _ ->  
+                        fileWriter.WriteLine()
                     fileWriter.Write(currLine)
                     currLineNumber <- currLineNumber + 1
                     if (currLineNumber >= maxLinesPerFile) then
                         fileWriter.Close()
                         fileWriter.Dispose()
                         currFileNumber <- currFileNumber + 1
-                        let newFileName = Path.GetFileNameWithoutExtension(outputFilepath) + "-" + currFileNumber.ToString() + Path.GetExtension(outputFilepath)
-                        let! newStream = folderPicked.CreateFile newFileName |> Async.AwaitTask
-                        let outputFilepath = Path.Combine( outputFolder, newFileName)
-                        fileWriter <- new StreamWriter(newStream)
-                        filesCreated.Add(outputFilepath)
-                        currFileNotifier  currFileNumber
                         currLineNumber <- 0
                     currLine <- fileReader.ReadLine()
                 fileWriter.Close()
